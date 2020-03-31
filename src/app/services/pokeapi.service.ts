e12eb66231e654dfb72dd372d16e810b9eec240f
lParanhos <leandro.paranhos@dextra-sw.com>
2020-03-31 08:12:39 -0300

    project

---
+++ src/app/services/pokeapi.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface PokemonListResponse {
+  created: string,
+  modified: string,
+  name: string,
+  pokemon: any[],
+  resource_uri: string
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeapiService {
+
+  private url = 'https://dev.treinaweb.com.br/pokeapi';
+  pokemonList = []
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  listAll() {
+    this.http.get<PokemonListResponse>(`${this.url}/pokedex/1`)
+      .subscribe(
+        response => {
+          response.pokemon.forEach(pokemon => {
+            pokemon.number = this.getNumberFromUrl(pokemon.resource_uri)
+          })
+          this.pokemonList = this.sortPokemon(response.pokemon)
+            .filter(pokemon => pokemon.number < 1000)
+        }
+      )
+  }
+
+  private getNumberFromUrl(url) {
+    return parseInt(url.replace(/.*\/(\d+)\/$/, '$1'));
+  }
+
+  private sortPokemon(pokemonList) {
+    return pokemonList.sort((a, b) => {
+      return (a.number > b.number ? 1 : -1);
+    })
+  }
+}
